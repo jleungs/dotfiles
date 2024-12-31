c1daee0f6ed8438ad4a5d556af0deab87c2b790a
Jonathan Leung <jl3@tuta.io>
2024-12-31 10:55:58 +0000

    blessed with new font

--- suckless/dwm-6.5/config.h
+++ suckless/dwm-6.5/config.h
@@ -11,8 +11,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus:size=10" };
-static const char dmenufont[]       = "Terminus:size=10";
+//static const char *fonts[]          = { "TempleOS:size=9" };
+static const char *fonts[]          = { "TempleOS:pixelsize=12:antialias=true:autohint=true" };
+//static const char dmenufont[]       = "TempleOS:size=9";
+static const char dmenufont[]       = "TempleOS:pixelsize=12:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
--- suckless/st-0.9.2/config.h
+++ suckless/st-0.9.2/config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminus:pixelsize=11:antialias=true:autohint=true";
+//static char *font = "Terminus:pixelsize=11:antialias=true:autohint=true";
+static char *font = "TempleOS:pixelsize=11:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
