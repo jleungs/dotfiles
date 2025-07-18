47c24c75406023f9004fc39b0bbf8b40a4ad60cb
Jonathan Leung <jl3@tuta.io>
2025-07-18 21:13:49 +0100

    updated for openbsd@

--- misc/low-bat_notify.sh
+++ misc/low-bat_notify.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+
 export DISPLAY=:0
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+#export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 export XAUTHORITY=/home/jl/.Xauthority
 
-BAT0=$(cat /sys/class/power_supply/BAT1/capacity)
+#BAT0=$(cat /sys/class/power_supply/BAT1/capacity)
+BAT0=$(apm -l)
 
-if [[ $BAT0 -lt 110 ]];then
-	/usr/bin/notify-send -u critical "=== LOW BATTERY ==="
+if [[ $BAT0 -lt 10 ]];then
+	notify-send -u critical "=== LOW BATTERY ==="
 fi
