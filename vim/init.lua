b73b00e0b8316f97e85cd971ee011d7b32d9b1fd
Jonathan Leung <jl3@tuta.io>
2026-01-11 21:41:24 +0000

    updated nvim conf, no plugins

--- vim/init.lua
+++ vim/init.lua
@@ -1,7 +1,9 @@
--- options
+-- Options
 vim.g.mapleader = ' '
+vim.cmd.filetype("plugin indent on")
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.numberwidth = 2
 vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
@@ -9,14 +11,13 @@ vim.opt.shiftwidth = 0
 vim.opt.termguicolors = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-vim.opt.scrolloff = 15
+vim.opt.scrolloff = 10
 vim.opt.spelllang = { 'en_gb' }
 vim.opt.ignorecase = true
--- remaps
+vim.opt.clipboard = "unnamedplus"
+-- Remaps
 vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('v', '<', '<gv')
-vim.keymap.set({'n', 'v'}, '<leader>y', [['+y]])
-vim.keymap.set('n', '<leader>Y', [['+Y]])
 -- Toggle 'colorcolumn' at column 80
 vim.keymap.set('n', '<leader>c', function()
     if vim.wo.colorcolumn == '' then
@@ -25,7 +26,7 @@ vim.keymap.set('n', '<leader>c', function()
         vim.opt.colorcolumn = ''
     end
 end, { silent = true })
--- colorscheme
+-- Colorscheme
 vim.cmd.colorscheme('quiet')
 vim.cmd.highlight({ 'Keyword', 'gui=bold' })
 vim.cmd.highlight({ 'Comment', 'gui=italic' })
@@ -33,132 +34,16 @@ vim.cmd.highlight({ 'Constant', 'guifg=#999999' })
 vim.cmd.highlight({ 'Normal', 'guibg=#000000' })
 vim.cmd.highlight({ 'Visual', 'guifg=#aaaaaa' })
 vim.cmd.highlight({ 'MatchParen', 'guifg=#ffffff' })
--- remember last cursor position
+-- Remember last cursor position
 vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
     pattern = { '*' },
     callback = function()
 	vim.api.nvim_exec2('silent! normal! g`"zv', { output = false })
     end,
 })
--- lazy plugin manager
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
-      { '\nPress any key to exit...' },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup({
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            require('telescope').setup()
-
-            -- set keymaps
-            local keymap = vim.keymap
-
-            keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Fuzzy find files in cwd' })
-            keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Fuzzy find recent files' })
-        end,
-    },
-    {
-        'neovim/nvim-lspconfig',
-        dependencies = { 'saghen/blink.cmp' },
-        config = function()
-            local capabilities = require('blink.cmp').get_lsp_capabilities()
-            -- Import the LSP config module
-            local lspconfig = require('lspconfig')
-            -- required for blink.cmp
-            lspconfig['pyright'].setup({ capabilities = capabilities })
-            -- Function to set up keymaps after LSP attaches to a buffer
-            local on_attach = function(_, bufnr)
-                local opts = { noremap = true, silent = true, buffer = bufnr }
-                vim.diagnostic.config({ virtual_text = true })
-                -- LSP key mappings
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)           -- Go to definition
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)          -- Go to declaration
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)           -- List references
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)       -- Go to implementation
-                vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)       -- Rename symbol
-                vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)         -- Go to previous diagnostic
-                vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)         -- Go to next diagnostic
-            end
-            -- language servers
-            lspconfig.pyright.setup({
-                on_attach = on_attach,
-                filetypes = {'python'},
-            })
-            lspconfig.texlab.setup({
-                on_attach = on_attach,
-                filetypes = {'latex'},
-            })
-            lspconfig.clangd.setup({
-                on_attach = on_attach,
-                filetypes = {'c', 'cpp'},
-            })
-            end,
-        },
-        {
-            'williamboman/mason.nvim',
-            config = function()
-                require('mason').setup({
-                    ui = {
-                        border = 'rounded', -- Set the border style for the Mason UI
-                        icons = {
-                            package_installed = '✓',
-                            package_pending = '➜',
-                            package_uninstalled = '✗'
-                        },
-                    },
-                    log_level = vim.log.levels.INFO,
-                    max_concurrent_installers = 4, -- Limit concurrent installations
-                })
-            end,
-        },
-        {
-            'williamboman/mason-lspconfig.nvim',
-            config = function()
-                require('mason-lspconfig').setup({
-                    ensure_installed = { 'pyright', 'texlab', 'clangd' }, -- Automatically install these servers
-                    automatic_installation = true,
-                })
-            end,
-        },
-        {
-            'saghen/blink.cmp',
-            dependencies = { 'rafamadriz/friendly-snippets' },
-            version = '*',
-
-            opts = {
-                keymap = { preset = 'default' },
-                -- (Default) Only show the documentation popup when manually triggered
-                completion = { documentation = { auto_show = false } },
-                -- Default list of enabled providers defined so that you can extend it
-                -- elsewhere in your config, without redefining it, due to `opts_extend`
-                sources = {
-                    default = { 'lsp', 'path', 'snippets', 'buffer' },
-                },
-                fuzzy = { implementation = 'prefer_rust_with_warning' }
-            },
-            opts_extend = { 'sources.default' }
-        },
-        {
-            'lervag/vimtex',
-            lazy = false,     -- we don't want to lazy load VimTeX
-            init = function()
-                -- VimTeX configuration goes here, e.g.
-                vim.g.vimtex_view_method = 'zathura'
-            end
-        }
-})
+-- Buffers mappings
+vim.keymap.set('n','<leader>%',':vsplit<CR>',{silent=true})
+vim.keymap.set('n','<C-h>','<C-w>h',{silent=true})
+vim.keymap.set('n','<C-j>','<C-w>j',{silent=true})
+vim.keymap.set('n','<C-k>','<C-w>k',{silent=true})
+vim.keymap.set('n','<C-l>','<C-w>l',{silent=true})
