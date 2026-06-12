bea3f22ff42c3bbf8c353ab1cfd6948a3cd0d6e5
Jonathan Leung <jl3@tuta.io>
2026-06-12 20:55:11 +0000

    cleaned up neovim config

--- vim/init.lua
+++ vim/init.lua
@@ -1,32 +1,43 @@
--- Options
+local opt = vim.opt
+local map = vim.keymap.set
 vim.g.mapleader = ' '
-vim.cmd.filetype("plugin indent on")
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.numberwidth = 2
-vim.opt.cursorline = true
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 0
-vim.opt.termguicolors = true
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.scrolloff = 10
-vim.opt.spelllang = { 'en_gb' }
-vim.opt.ignorecase = true
--- Remaps
-vim.keymap.set('v', '>', '>gv')
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '<leader>y', '"+y', { noremap = true })
-vim.keymap.set('n', '<leader>Y', '"+Y', { noremap = true })
--- Toggle 'colorcolumn' at column 80
-vim.keymap.set('n', '<leader>c', function()
-    if vim.wo.colorcolumn == '' then
-        vim.opt.colorcolumn = '80'
-    else
-        vim.opt.colorcolumn = ''
-    end
+-- Interface
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 2
+opt.cursorline = true
+opt.scrolloff = 10
+opt.termguicolors = true
+-- Indentation
+opt.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 0       -- Use tabstop
+opt.softtabstop = -1     -- Use effective shiftwidth
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+-- Files and sessions
+opt.undofile = true
+opt.confirm = true
+-- Splits
+opt.splitright = true
+opt.splitbelow = true
+-- Spelling dictionary; spell checking remains disabled until toggled
+opt.spelllang = { 'en_gb' }
+-- Preserve selection after indentation
+map('x', '>', '>gv')
+map('x', '<', '<gv')
+-- Explicit system-clipboard yanks
+map({ 'n', 'x' }, '<leader>y', '"+y')
+map('n', '<leader>Y', '"+Y')
+-- Toggle column marker in the current window
+map('n', '<leader>c', function()
+    vim.wo.colorcolumn =
+        vim.wo.colorcolumn == '' and '80' or ''
 end, { silent = true })
+-- Optional spelling toggle
+map('n', '<leader>s', '<cmd>setlocal spell!<cr>', {silent = true})
 -- Colorscheme
 vim.cmd.colorscheme('quiet')
 vim.cmd.highlight({ 'Keyword', 'gui=bold' })
@@ -35,16 +46,20 @@ vim.cmd.highlight({ 'Constant', 'guifg=#999999' })
 vim.cmd.highlight({ 'Normal', 'guibg=#000000' })
 vim.cmd.highlight({ 'Visual', 'guifg=#aaaaaa' })
 vim.cmd.highlight({ 'MatchParen', 'guifg=#ffffff' })
--- Remember last cursor position
-vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
-    pattern = { '*' },
-    callback = function()
-	vim.api.nvim_exec2('silent! normal! g`"zv', { output = false })
+-- Restore the last valid cursor position
+vim.api.nvim_create_autocmd('BufReadPost', {
+    callback = function(args)
+        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.api.nvim_win_set_cursor(0, mark)
+            vim.cmd('normal! zv')
+        end
     end,
 })
--- Buffers mappings
-vim.keymap.set('n','<leader>%',':vsplit<CR>',{silent=true})
-vim.keymap.set('n','<C-h>','<C-w>h',{silent=true})
-vim.keymap.set('n','<C-j>','<C-w>j',{silent=true})
-vim.keymap.set('n','<C-k>','<C-w>k',{silent=true})
-vim.keymap.set('n','<C-l>','<C-w>l',{silent=true})
+-- Window management
+map('n', '<leader>%', '<cmd>vsplit<cr>', {silent = true})
+map('n', '<C-h>', '<C-w>h', { desc = 'Window left' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Window below' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Window above' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Window right' })
